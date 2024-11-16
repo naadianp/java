96b6d67bc10939aa5d6db8982507cd562e935f18
naadianp <nadia.nur7806@smk.belajar.id>
2024-11-16 13:11:50 +0700

    Add files via upload

---
+++ empty.java
@@ -0,0 +1,10 @@
+class cek {
+    public static void main(String[] args) {
+        
+        String name1 = "";
+        String name2 = "hallo";
+        
+        System.out.println(name1.isEmpty());
+        System.out.println(name2.isEmpty());
+    }
+}
